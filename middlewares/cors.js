e5ec2943178bd122268d8d106f9ba32d4851e227
Ivan Nikolaev <149399445+0bobux@users.noreply.github.com>
2024-04-18 17:01:31 +0300

    Update cors.js
    исправил ошибку с cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -8,10 +8,10 @@ function cors(req, res, next) {
     const { origin } = req.headers; // Смотрим, кто стучится к серверу
 
     if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header('Access-Control-Allow-Origin', '*'); // Говорим: «Добро пожаловать!»
+        res.header('Access-Control-Allow-Origin', origin); // Говорим: «Добро пожаловать!»
     }
 
     next(); // Идём дальше, не задерживаем очередь
 }
 
-module.exports = { cors };
+module.exports = cors;
